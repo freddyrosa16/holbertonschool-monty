840a063c2a2e9351dbe0b279a5cca09a781c4993
freddyrosa16 <freddy.rosa16@gmail.com>
2024-01-05 11:04:40 -0400

    Deletion

--- arguments.c
+++
@@ -1,9 +0,0 @@
-#include "monty.h"
-void arguments(int argc)
-{
-	if (argc == 2)
-		return;
-
-	dprintf(2, "USAGE: monty file\n");
-	exit(EXIT_FAILURE);
-}
--- main.c
+++
@@ -1,9 +0,0 @@
-#include "monty.h"
-int main(int argc, char **argv)
-{
-	(void) argv;
-
-	validate_arguments(argc);
-
-	return (0);
-}
